5210761da0df1acc0c3afae4d69562b918a05c4c
PETEROLO291 <peterjansimonsmerino@gmail.com>
2021-02-03 18:22:05 +0100

    1.1
    Bug Fixes

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from threading import Thread
 # Made By PETEROLO 291©
 
 
+# Custop dark theme configuration
 sg.LOOK_AND_FEEL_TABLE['CustomDarkTheme'] = {'BACKGROUND': '#373737',
                                         'TEXT': '#FFFFFF',
                                         'INPUT': '#474747',
@@ -68,7 +69,7 @@ except:
 
 #··Window Elements··#
 
-
+# Custop popup window to configure the Start and Stop keys
 def key_popup(window):
     global start_key, stop_key, values
     
@@ -83,31 +84,37 @@ def key_popup(window):
                                             }
 
 
+    # Set popup theme
     sg.theme("CustomDarkTheme")
 
+
+    # Popup layout
     layout = [  [sg.Text("Start Key", pad=((0, 13), None), font=("Arial", 13)), sg.Text("Stop Key", font=("Arial", 13))],
                 [sg.Input(default_text=start_key ,size=(3, 1), font=("Arial", 15), pad=((0, 40), None), key="-K1-"), sg.Input(default_text=stop_key, size=(3, 1), font=("Arial", 15), key="-K2-")],
                 [sg.Button("Save", size=(10, 1), pad=((0, 0), 8), font=("Arial", 11))]]
 
 
+    # Popup window construction
     pop = sg.Window('HotKeys', layout, size=(250, 110), element_justification="C", icon="flame_ico.ico", finalize=True)
 
+    # Popup event reading
     event, values = pop.read()
 
+    # If Save button is pressed:
     if event == "Save":
-        start_key = str(values["-K1-"])
+        start_key = str(values["-K1-"]) # Save the input values into a variable
         stop_key = str(values["-K2-"])
 
-        window["-STAB-"].update(f"Start ({start_key.upper()})")
+        window["-STAB-"].update(f"Start ({start_key.upper()})") # Edit start and stop button text
         window["-STOB-"].update(f"Start ({stop_key.upper()})")
 
     with open("start_key.txt", "w+") as save_start_key:
-        save_start_key.write(start_key.upper())
+        save_start_key.write(start_key.upper()) # Save start key in txt file
         save_start_key.close()
 
 
     with open("stop_key.txt", "w+") as save_stop_key:
-        save_stop_key.write(stop_key.upper())
+        save_stop_key.write(stop_key.upper()) # Save stop key in txt file
         save_stop_key.close()
 
     
@@ -115,9 +122,9 @@ def key_popup(window):
     pop.close()
     pop.write_event_value(event, None)
 
-
+# Frame layout
 frame = [   [sg.Text("Click Interval (Seconds):", font=("Arial", 13), pad=((0, 75), None)), sg.Text("Repeat x Times:", font=("Arial", 13), pad=((0, 25), None))],
-            [sg.Input(size=(30, 1), justification="l", default_text=0.1, key="-I1-"), sg.Input(size=(30, 1), justification="l", default_text=1000, key="-I2-")],
+            [sg.Input(size=(30, 1), justification="c", default_text=0.0, key="-I1-"), sg.Input(size=(30, 1), justification="c", default_text=1000, key="-I2-")],
             [sg.InputOptionMenu(('Left Click', 'Right Click'), key="-IOM-", pad=((15, 100), 2)), sg.Checkbox('Click until stoped', key="-CB-")],
             [sg.Text("—" * 1000)],
             [sg.Button(f"Start ({start_key})", font=("Arial", 15), pad=(5, (0, 0)), key="-STAB-"), sg.Button(f"Stop ({stop_key})", font=("Arial", 15), pad=(5, (0, 0)), key="-STOB-")],
@@ -125,7 +132,7 @@ frame = [   [sg.Text("Click Interval (Seconds):", font=("Arial", 13), pad=((0, 7
             [sg.Text("By PETEROLO 291", font=("Arial", 8), pad=(None, (10, 0)))]]
 
 
-# Putting layout inside a frame is just to add a super small margin that i whanted to add
+# Put layout inside a frame is just to add a super small margin that i whanted to add
 layout = [  [sg.Text("Flame Auto Clicker", justification='c', size=(100, 1), pad=(None, (10, 0)) , font=("Freestyle Script", 35))],
             [sg.Text("—" * 1000)],
             [sg.Frame(None, frame, element_justification='c', border_width=0)]]
@@ -136,13 +143,15 @@ layout = [  [sg.Text("Flame Auto Clicker", justification='c', size=(100, 1), pad
 window = sg.Window('Flame Auto Clicker', layout, size=(530, 310), finalize=True, icon="flame_ico.ico", keep_on_top=False, element_justification="c", margins=(0, 0))
 
 
+# Input details config
 window['-I1-'].Widget.configure(highlightcolor='#FFFFFF', highlightbackground="#9C9C9C", insertbackground="White", highlightthickness=1)
 window['-I2-'].Widget.configure(highlightcolor='#FFFFFF', highlightbackground="#9C9C9C", insertbackground="White", highlightthickness=1)
 
 
-# Backend
+# ]---Backend---[
 
 
+# Loop where the click events are created
 def click_loop():
     global clicking, delay, cb_marked, repeat
 
@@ -172,7 +181,6 @@ def click_loop():
             sleep(float(delay))
 
 
-st_click_loop = Thread(target=click_loop)
 
 
 
@@ -194,21 +202,23 @@ def detect_keys():
             except:
                 pass
 
+# Creation of threads
 start_k_check = Thread(target=detect_keys)
-
+st_click_loop = Thread(target=click_loop)
 
 
 
 #···Main PySimpleGui event loop···#
 while running:
 
+    # Start threads
     if threads_started == False:
         start_k_check.start()
         st_click_loop.start()
         threads_started = True
 
     event, values = window.read(timeout=250)
-
+    
     if start_key == "" or stop_key == "":
         start_key = "F6"
         stop_key = "F4"
@@ -269,7 +279,7 @@ while running:
 
         if values["-IOM-"] == "Left Click":
             click_but = "left"
-
+        # -IOM- (Input Option Menu)
         if values["-IOM-"] == "Right Click":
             click_but = "right"
 
@@ -279,10 +289,10 @@ while running:
         if values["-CB-"] == False:
             cb_marked = False
 
-        if event == "Start (F6)":
+        if event == "-STAB-":
             start_pressed = True
         
-        if event == "Stop (F4)":
+        if event == "-STOB-":
             stop_pressed = True
     except:
         pass
